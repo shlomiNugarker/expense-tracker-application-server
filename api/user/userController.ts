a260447d1b6fb6789a43572c4fb3dc0e1948e189
Shlomi Nugarker <shlomin1231@gmail.com>
2024-06-08 14:51:52 +0300

    user controller

--- api/user/userController.ts
+++ api/user/userController.ts
@@ -2,41 +2,18 @@ import userService from './userService'
 import { Request, Response } from 'express'
 
 export default {
-  getUser,
-  getUsers,
-  deleteUser,
+  getUserByEmail,
   updateUser,
   addUser,
 }
 
-async function getUser(req: Request, res: Response) {
+async function getUserByEmail(req: Request, res: Response) {
   try {
-    const user = await userService.getById(req.params.id)
+    const user = await userService.getByEmail(req.params.email)
     res.send(user)
   } catch (err) {
-    console.log('Failed to get user', err)
+    console.error(err)
     res.status(500).send({ err: 'Failed to get user' })
-  }
-}
-
-async function getUsers(req: Request, res: Response) {
-  try {
-    const filterBy = req.query
-    const users = await userService.query(filterBy)
-    res.send(users)
-  } catch (err) {
-    console.log('Failed to get users', err)
-    res.status(500).send({ err: 'Failed to get users' })
-  }
-}
-
-async function deleteUser(req: Request, res: Response) {
-  try {
-    await userService.remove(req.params.id)
-    res.send({ msg: 'Deleted successfully' })
-  } catch (err) {
-    console.log('Failed to delete user', err)
-    res.status(500).send({ err: 'Failed to delete user' })
   }
 }
 
@@ -46,7 +23,7 @@ async function updateUser(req: Request, res: Response) {
     const savedUser = await userService.update(user)
     res.send(savedUser)
   } catch (err) {
-    console.log('Failed to update user', err)
+    console.error(err)
     res.status(500).send({ err: 'Failed to update user' })
   }
 }
@@ -57,7 +34,7 @@ async function addUser(req: Request, res: Response) {
     const savedUser = await userService.add(user)
     res.send(savedUser)
   } catch (err) {
-    console.log('Failed to update user', err)
+    console.error(err)
     res.status(500).send({ err: 'Failed to update user' })
   }
 }
