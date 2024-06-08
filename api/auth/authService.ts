2b1f7c4ef20acb99b0f4ad16babae347d3d21c4b
Shlomi Nugarker <shlomin1231@gmail.com>
2024-06-08 14:52:19 +0300

    auth service

--- api/auth/authService.ts
+++ api/auth/authService.ts
@@ -6,25 +6,32 @@ export default {
   login,
 }
 
-async function login(userName: string, password: string) {
-  const user = await userService.getByUsername(userName)
-  if (!user) return Promise.reject('Invalid userName or password')
-  const match = await bcrypt.compare(password, user.password)
-  if (!match) return Promise.reject('Invalid userName or password')
-  delete user.password
-  return user
+async function login(email: string, password: string) {
+  try {
+    const user = await userService.getByEmail(email)
+
+    if (!user) return Promise.reject('Invalid email or password')
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) return Promise.reject('Invalid email or password')
+
+    delete user.password
+    return user
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-async function signup(
-  userName: string,
-  password: string,
-  fullName: string,
-  isMentor: boolean
-) {
-  const saltRounds = 10
-  if (!userName || !password || !fullName)
-    return Promise.reject('fullName, userName and password are required!')
+async function signup(email: string, password: string, fullName: string) {
+  try {
+    const saltRounds = 10
+    if (!email || !password || !fullName)
+      return Promise.reject('fullName, email and password are required!')
 
-  const hash = await bcrypt.hash(password, saltRounds)
-  return userService.add({ userName, password: hash, fullName, isMentor })
+    const hash = await bcrypt.hash(password, saltRounds)
+    return userService.add({ email, password: hash, fullName })
+  } catch (err) {
+    console.error(err)
+  }
 }
